e99fe044c568bc015dfb3e94ef2bb44f4386d124
laurianderson <laura.anderson@mercadolibre.com>
2023-03-31 09:16:09 -0300

    feat: add exercise 1, using go gin

---
+++ class1_31march/exercise_1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//Crea un router con gin 
+	router := gin.Default()
+
+	//Captura la solicitud get hello world
+	router.GET("/hello-world", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello world!",
+		})
+    })
+
+	//Corremos nuestro servidor sobre el puerto 8080
+	router.Run()
+}
+
+
